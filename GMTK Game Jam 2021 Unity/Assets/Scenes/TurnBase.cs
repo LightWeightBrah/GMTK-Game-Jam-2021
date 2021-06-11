c6080366781fcaa9e15cf59f62915022ea96d899
MatherPL <61986656+MatherPL@users.noreply.github.com>
2021-06-11 23:33:11 +0200

    Turn

---
+++ GMTK Game Jam 2021 Unity/Assets/Scenes/TurnBase.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnBase : MonoBehaviour
+{
+
+    public int Turn=0;
+    public int TurnCounter;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Turns();
+    }
+
+
+    private void Turns()
+    {
+        if (Turn==0)
+        {
+            PlayerTurn();
+        }
+        else
+        {
+            EnemyTurn();
+        }
+    }
+    private void PlayerTurn()
+    {
+        //PlayerTurn
+        
+    }
+    private void EnemyTurn()
+    {
+        //PlayerTurn
+
+    }
+
+    public void PlayerTurnEnd()
+    {
+        if (Turn==0)
+        {
+            //PlayerTurnEnd
+            //Card[].Cooldown --;
+            Turn++;
+            Debug.Log("PlayerEndTurn");
+        }
+    }
+    public void EnemyTurnEnd()
+    {
+        if (Turn==1)
+        {
+            //EnemyTurnEnd
+            Turn--;
+            TurnCounter++;
+        }
+
+    }
+}
