992d159028d41ca65a597cb69140f12f16cf0b00
MatherPL <61986656+MatherPL@users.noreply.github.com>
2021-06-12 09:47:21 +0200

    Update

---
+++ GMTK Game Jam 2021 Unity/Assets/Scenes/MainMenu/MenuManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour
+{
+    public string scenename;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(scenename);
+    }
+}
