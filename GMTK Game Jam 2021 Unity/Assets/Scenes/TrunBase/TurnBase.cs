19fe8e26f736a3e5c4cf5af8d0b84ebcec60b2f5
MatherPL <61986656+MatherPL@users.noreply.github.com>
2021-06-12 12:22:35 +0200

    TurnBasedCombat

--- GMTK Game Jam 2021 Unity/Assets/Scenes/TrunBase/TurnBase.cs
+++ GMTK Game Jam 2021 Unity/Assets/Scenes/TrunBase/TurnBase.cs
@@ -1,63 +1,119 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+public enum BattleState
+{
+    PlayerTurn,
+    PlayerAction,
 
+    EnemyTurn,
+    EnemyAction,
+
+    Won,
+    Lost
+}
 public class TurnBase : MonoBehaviour
 {
-    public enum BattleState
-    {
-        PlayerTurn,
-        EnemyTurn
-    }
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerStart;
+    public Transform enemyStart;
+
     public BattleState battleState = BattleState.PlayerTurn;
     public int turnCounter;
 
-    void Update()
+    public BattleHud playerHud;
+    public BattleHud enemyHud;
+
+    private void Start()
     {
-        Turns();
+        StartBattle();
     }
 
-
-    private void Turns()
+    void StartBattle()
     {
-        if (battleState == BattleState.PlayerTurn)
-        {
-            PlayerTurn();
-        }
-        else
-        {
-            EnemyTurn();
-        }
+        GameObject player = Instantiate(playerPrefab, playerStart);
+        //playerUnit=player.GetComponent<>();
+
+        GameObject enemy = Instantiate(enemyPrefab, enemyStart);
+
+        // playerHud.HudSettings();
+        // enemyHud.HudSettings();
+
+        battleState = BattleState.PlayerTurn;
+        PlayerTurn();
     }
     private void PlayerTurn()
     {
-        //PlayerTurn
+        //Player Add cards
+        //bool isDead = Enemy.TakeDamage(player.damgae)
+        Debug.Log("PlayerT");
 
     }
-    private void EnemyTurn()
-    {
-        //PlayerTurn
 
-    }
 
     public void PlayerTurnEnd()
     {
         if (battleState == BattleState.PlayerTurn)
         {
             //PlayerTurnEnd
+            //enemyHud.SetHp(enemy.currentHP);
 
-            battleState++;
             Debug.Log("PlayerEndTurn");
+            /*
+            if (isDead)
+            {
+                battleState = BattleState.Won;
+                EndBattle();
+            }
+            else
+            {
+                battleState = BattleState.EnemyTurn;
+                EnemyTurn();
+            }
+            */
+
         }
+    }
+    private void EnemyTurn()
+    {
+        //EnemyTurn
+        //bool isDead =player.TakeDamage(enemy.damage);
+        // playerHud.SetHp(player.currentHP);
+
     }
     public void EnemyTurnEnd()
     {
         if (battleState == BattleState.EnemyTurn)
         {
             //EnemyTurnEnd
-            battleState--;
-            turnCounter++;
+            /*
+            if (isDead)
+            {
+                battleState = BattleState.Lost;
+                EndBattle();
+            }
+            else
+            {
+                battleState = BattleState.PlayerTurn;
+                PlayerTurn();
+            }
+            */            
         }
 
+    }
+
+    void EndBattle()
+    {
+        if (battleState == BattleState.Won)
+        {
+            //win
+        }
+        else if (battleState == BattleState.Lost)
+        {
+
+        }
     }
 }
